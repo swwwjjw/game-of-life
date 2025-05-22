e489a33c1ca743e297dfc64b2d33b2485723dc2b
swwwjjw <zhukegor2006@gmail.com>
2025-05-22 16:19:43 +0300

    Update GameOfLife.h

--- GameOfLife.h
+++ GameOfLife.h
@@ -1,16 +1,3 @@
 ﻿#pragma once
 #include <iostream>
 #include <SDL.h>
-
-//class Button {
-//public:
-//	Button() {
-//		//
-//	}
-//	void HandleEvent(SDL_Event& event) {
-//
-//	}
-//	void Render(SDL_Renderer* renderer) {
-//
-//	}
-//};
